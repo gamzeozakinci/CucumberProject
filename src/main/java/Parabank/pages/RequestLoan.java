04f4773194b73884b9099636d80743c2a2b5a669
Quentinine <gamze.ozakinci@gmail.com>
2026-06-24 22:16:57 +0300

    4.commit

---
+++ src/main/java/Parabank/pages/RequestLoan.java
@@ -0,0 +1,34 @@
+package Parabank.pages;
+
+import Parabank.utils.GWD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class RequestLoan {
+    public RequestLoan() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//*[@href='requestloan.htm']")
+    public WebElement openAccountButton;
+
+    @FindBy(css = "input#amount")
+    public WebElement loanAmount;
+
+    @FindBy(css = "input#downPayment")
+    public WebElement downPayment;
+
+    @FindBy(css = "input.button")
+    public WebElement applyButton;
+
+    @FindBy(css = "#customer.address.city")
+    public WebElement city;
+
+    @FindBy(css = "#customer.address.state")
+    public WebElement state;
+
+    @FindBy(css = "input.button")
+    public WebElement updateButton;
+}
+// oncesinde hesaba para atmak lazım
