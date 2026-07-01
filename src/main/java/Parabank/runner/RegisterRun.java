514b4155cfba5f9fb0de326230fa71b610c0f97e
Quentinine <gamze.ozakinci@gmail.com>
2026-07-01 21:02:41 +0300

    8.commit

---
+++ src/main/java/Parabank/runner/RegisterRun.java
@@ -0,0 +1,16 @@
+package Parabank.runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "src/test/java/features/Tests",
+        glue = "stepDefinitions"
+)
+
+public class RegisterRun extends AbstractTestNGCucumberTests {
+
+
+
+}
